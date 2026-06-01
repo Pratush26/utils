5a6777f1d40031bb67b97945e7a28f27d6b51962
Pratush Barua <pratushbarua26@gmail.com>
2026-06-01 20:02:33 +0600

    updated constants

---
+++ functions/constants.ts
@@ -0,0 +1,23 @@
+export const companyName = "Example";
+// export const companyFullName = "Example Pte Ltd";
+export const address = "example road, example city example country";
+export const contactNo = "+xxxxxxxxxxx";
+export const contactStr = "+xx xxxxx xxxx";
+export const e_mail = "example@gmail.com";
+// export const whatsAppNo = "+xxxxxxxxxxx";
+export const mainServiceName = "";
+export const whatsAppMessage = `Hello! I am interested in your ${mainServiceName? mainServiceName+" " : ""}services. Can you help me?`;
+export const whatsAppUrl = `https://wa.me/${contactNo}?text=${encodeURIComponent(whatsAppMessage)}`;
+
+// social links
+export const fb_link = "https://www.facebook.com/";
+export const insta_link = "https://www.instagram.com/";
+export const tiktok_link = "https://www.tiktok.com/";
+export const linkdin_link = "https://www.linkedin.com/company/";
+export const youTube_link = "https://www.youtube.com/";
+export const discord_link = "https://discord.com/";
+export const x_link = "https://x.com/";
+export const git_link = "https://github.com/";
+export const website_url = "www.example.com";
+
+export const mapStr = "road+city+,+country";
